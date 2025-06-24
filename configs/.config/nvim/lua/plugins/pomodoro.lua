9a84b97c4a5d15343697c9c54c8e442f9af786d9
[tariqbaater] <[tariqbaater@gmail.com]>
2025-06-24 09:33:52 +0300

    removed unnecessary garbage

--- configs/.config/nvim/lua/plugins/pomodoro.lua
+++ configs/.config/nvim/lua/plugins/pomodoro.lua
@@ -1,13 +1,10 @@
 return {
-    "epwalsh/pomo.nvim",
-    version = "*", -- Recommended, use latest release instead of latest commit
-    lazy = true,
-    cmd = { "TimerStart", "TimerRepeat" },
-    dependencies = {
-        -- Optional, but highly recommended if you want to use the "Default" timer
-        "rcarriga/nvim-notify",
-    },
-    opts = {
-        -- See below for full list of options 👇
-    },
+  "epwalsh/pomo.nvim",
+  version = "*",   -- Recommended, use latest release instead of latest commit
+  lazy = true,
+  cmd = { "TimerStart", "TimerRepeat" },
+  dependencies = {
+    -- Optional, but highly recommended if you want to use the "Default" timer
+    "rcarriga/nvim-notify",
+  },
 }
