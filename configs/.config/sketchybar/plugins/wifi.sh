33d0e47a2d8d1e714bb9102a804f258a655de38e
[tariqbaater] <[tariqbaater@gmail.com]>
2026-02-09 01:50:27 +0300

    added borders around the sketchybar icons

--- configs/.config/sketchybar/plugins/wifi.sh
+++ configs/.config/sketchybar/plugins/wifi.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #:TODO: Add support for multiple WiFi interfaces and better error handling.
+
 # Simple WiFi status check
 WIFI_STATUS=$(ifconfig en0 2>/dev/null | grep "status:" | awk '{print $2}')
 
