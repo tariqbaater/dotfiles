0510a4397202ad40032357053bb73f657703fc7f
[tariqbaater] <[tariqbaater@gmail.com]>
2026-02-11 08:15:33 +0300

    fix: when generating the brew list for cask append '--cask' flag.

--- bash/bash_scripts/brew_list.sh
+++ bash/bash_scripts/brew_list.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Installed Homebrew formulas (commands):"
-brew list --formula
-
-echo ""
-echo "Installed Homebrew casks:"
-brew list --cask
+brew list --formula > /Users/tariq/bash_scripts/brew_list
+# append "--cask" to the cask list and append it to the brew_list file
+brew list --cask | sed 's/^/--cask /' >> /Users/tariq/bash_scripts/brew_list
